823e5dc481a97d6373353a346ac7b95bf7d2500a
Keo <41642242+MadameMalady@users.noreply.github.com>
2023-07-15 21:02:03 -0400

    Update build.sh

--- My Waydroid Manager/scripts/build.sh
+++ My Waydroid Manager/scripts/build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 if [ $(hostname) = 'mobian' ]; then
-  sudo apt install curl ca-certificates -y
-  curl https://repo.waydro.id | sudo bash
-  sudo apt install waydroid -y
-  echo "Please Launch waydroid from it's application icon, minimize osk"
+   sudo apt install curl ca-certificates -y
+   curl https://repo.waydro.id | sudo bash
+   sudo apt install waydroid -y
+   echo "Please Launch waydroid from it's application icon, minimize osk"
+elif [ $(hostname) = 'ubuntu' ]; then
+   sudo apt install curl ca-certificates -y
+   curl https://repo.waydro.id | sudo bash
+   sudo apt install waydroid -y
+   echo "Please Launch waydroid from it's application icon, minimize osk"
 elif [ $(hostname) = 'manjaro' ]; then
-  sudo pacman -Syyu waydroid
-  waydroid
-elif [ $(hostname) = 'postmarketos' ]; then
-  echo "Sorry, this option's still a work in progress
+   sudo pacman -Syyu waydroid
+   waydroid
+elif [ $(hostname) = 'postmarketos*' ]; then
+   echo "Sorry, this option's still a work in progress"
+else
+   echo "Error 404"
 fi
