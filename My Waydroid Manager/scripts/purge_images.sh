657d30164968dd9f84817b66b4462cc019b08771
Keo <41642242+MadameMalady@users.noreply.github.com>
2023-07-18 18:36:26 -0400

    Add files via upload

---
+++ My Waydroid Manager/scripts/purge_images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $(hostname) = 'mobian' ]; then
+   notify-send "My Waydroid Manager" "Check Your Terminal"
+   sudo rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid
+   exit
+elif [ $(hostname) = 'ubuntu' ]; then
+   notify-send "My Waydroid Manager" "Check Your Terminal"
+   sudo rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid
+   exit
+elif [ $(hostname) = 'manjaro' ]; then
+   echo "to be filled"
+elif [ $(hostname) = 'postmarketos*' ]; then
+   echo "to be filled" 
+else
+   echo "Error 404"
+   sleep 3
+   exit
+fi
+
