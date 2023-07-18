61ea71dd2fbc4bcef31908e97d62fc016df661b7
Keo <41642242+MadameMalady@users.noreply.github.com>
2023-07-18 18:36:08 -0400

    Update purge.sh
    Options are filling out more

--- My Waydroid Manager/scripts/purge.sh
+++ My Waydroid Manager/scripts/purge.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 if [ $(hostname) = 'mobian' ]; then
-   echo "to be filled"
+   notify-send "My Waydroid Manager" "Check Your Terminal"
+   waydroid session stop
+   sudo waydroid container stop
+   sudo apt remove waydroid
+   exit
 elif [ $(hostname) = 'ubuntu' ]; then
-   echo "to be filled"
+   notify-send "My Waydroid Manager" "Check Your Terminal"
+   waydroid session stop
+   sudo waydroid container stop
+   sudo apt remove waydroid
+   exit
 elif [ $(hostname) = 'manjaro' ]; then
    echo "to be filled"
 elif [ $(hostname) = 'postmarketos*' ]; then
    echo "to be filled" 
 else
    echo "Error 404"
+   sleep 3
+   exit
 fi
