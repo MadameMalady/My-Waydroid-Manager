5a1d640b2c59b7f91f3696e659814ab1d2ffb0fe
Keo <41642242+MadameMalady@users.noreply.github.com>
2023-07-27 15:54:14 -0400

    Update purge_images.sh

--- My Waydroid Manager/scripts/purge_images.sh
+++ My Waydroid Manager/scripts/purge_images.sh
@@ -3,11 +3,11 @@
 if [ $(hostname) = 'mobian' ]; then
    notify-send "My Waydroid Manager" "Check Your Terminal"
    sudo rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid
-   exit
+   notify-send "My Waydroid Manager" "Done!"
 elif [ $(hostname) = 'ubuntu' ]; then
    notify-send "My Waydroid Manager" "Check Your Terminal"
    sudo rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid
-   exit
+   notify-send "My Waydroid Manager" "Done!"
 elif [ $(hostname) = 'manjaro' ]; then
    echo "to be filled"
 elif [ $(hostname) = 'postmarketos*' ]; then
@@ -15,6 +15,4 @@ elif [ $(hostname) = 'postmarketos*' ]; then
 else
    echo "Error 404"
    sleep 3
-   exit
 fi
-
