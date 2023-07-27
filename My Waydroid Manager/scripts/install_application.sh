7e59264a59d3c38151dc07c18ad78d67968d4fe8
Keo <41642242+MadameMalady@users.noreply.github.com>
2023-07-27 19:54:50 +0000

    Add files via upload

---
+++ My Waydroid Manager/scripts/install_application.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+user="$(whoami)"
+
+if [ $(hostname) = 'mobian' ]; then
+   
+   notify-send "My Waydroid Manager" "Check Your Terminal"
+   
+   # copy application directory
+   cd ..
+   mkdir -p /home/$user/MyWaydroidManager
+   cp My_Waydroid_Manager.py /home/$user/MyWaydroidManager
+   cp -r icons /home/$user/MyWaydroidManager
+   cd icons
+   cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons/hicolor/scalable/apps
+   cp -p app.Keo.MyWaydroidManager.svg ~/.local/share/icons
+   cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons
+   cd ..
+   cp -r scripts /home/$user/MyWaydroidManager
+   cd  ~/.local/share/applications 
+   touch MyWaydroidManager.desktop
+   echo "
+[Desktop Entry]
+Version=0.1.9
+Name=My Waydroid Manager
+Comment=Manage Waydroid installations easily
+Exec=/usr/bin/env python3 /home/$user/MyWaydroidManager/My_Waydroid_Manager.py
+Icon=/home/$user/MyWaydroidManager/icons/app.Keo.MyWaydroidManager.svg
+X-Purism-FormFactor=Workstation;Mobile;
+Terminal=true
+Type=Application">MyWaydroidManager.desktop
+   notify-send "My Waydroid Manager" "Done!"
+elif [ $(hostname) = 'ubuntu' ]; then
+   
+   notify-send "My Waydroid Manager" "Check Your Terminal"
+   
+   # copy application directory
+      cd ..
+   mkdir -p /home/$user/MyWaydroidManager
+   cp My_Waydroid_Manager.py /home/$user/MyWaydroidManager
+   cp -r icons /home/$user/MyWaydroidManager
+   cd icons
+   cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons/hicolor/scalable/apps
+   cp -p app.Keo.MyWaydroidManager.svg ~/.local/share/icons
+   cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons
+   cd ..
+   cp -r scripts /home/$user/MyWaydroidManager
+   cd  ~/.local/share/applications 
+   touch MyWaydroidManager.desktop
+   echo "
+[Desktop Entry]
+Version=0.1.9
+Name=My Waydroid Manager
+Comment=Manage Waydroid installations easily
+Exec=/usr/bin/env python3 /home/$user/MyWaydroidManager/My_Waydroid_Manager.py
+Icon=/home/$user/MyWaydroidManager/icons/app.Keo.MyWaydroidManager.svg
+X-Purism-FormFactor=Workstation;Mobile;
+Terminal=true
+Type=Application">MyWaydroidManager.desktop
+   notify-send "My Waydroid Manager" "Done!"
+elif [ $(hostname) = 'manjaro' ]; then
+   echo "to be filled"
+elif [ $(hostname) = 'postmarketos*' ]; then
+   echo "to be filled" 
+else
+   echo "Error 404"
+   sleep 3
+fi
+
