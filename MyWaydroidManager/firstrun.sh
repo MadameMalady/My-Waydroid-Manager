4469a57326dcbd79804fd18ffc4bebe6957e20c2
RobertofLocksley <140762293+RobertofLocksley@users.noreply.github.com>
2023-07-28 16:07:55 -0300

    fixed the firstrun.sh firstrun.sh will no longer require to run the initial script with sudo

--- MyWaydroidManager/firstrun.sh
+++ MyWaydroidManager/firstrun.sh
@@ -1,4 +1,5 @@
 echo "This operation will require your password"
+sudo -v
 sudo chmod +x My_Waydroid_Manager.py
 cd scripts
 sudo chmod +x build.sh dependencies.sh install_application.sh purge.sh purge_images.sh
