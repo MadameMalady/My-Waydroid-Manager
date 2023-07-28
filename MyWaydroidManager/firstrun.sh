321f19417200099409479810f63174daa71947a9
RobertofLocksley <140762293+RobertofLocksley@users.noreply.github.com>
2023-07-28 15:48:01 -0300

    Added a "firstrun.sh" script added a script to simplify user installation. The script will make the necessary scripts executable and launch the My_Waydroid_manager.py Script

---
+++ MyWaydroidManager/firstrun.sh
@@ -0,0 +1,6 @@
+echo "This operation will require your password"
+sudo chmod +x My_Waydroid_Manager.py
+cd scripts
+sudo chmod +x build.sh dependencies.sh install_application.sh purge.sh purge_images.sh
+cd ..
+./My_Waydroid_Manager.py
