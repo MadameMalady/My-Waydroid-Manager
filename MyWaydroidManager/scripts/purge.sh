2b0e4fc27b3141fd53cdf8f6368522db3e3188a1
RobertofLocksley <ernstjunk@tutanota.com>
2023-07-28 20:54:31 -0300

    changed the purge.sh file to auto-uninstall this is subjective, however this uninstalls waydroid without having to navigate to the terminal and pressing y by adding -y to the uninstall command

--- MyWaydroidManager/scripts/purge.sh
+++ MyWaydroidManager/scripts/purge.sh
@@ -4,13 +4,13 @@ if [ $(hostname) = 'mobian' ]; then
    notify-send "My Waydroid Manager" "Check Your Terminal"
    waydroid session stop
    sudo waydroid container stop
-   sudo apt remove waydroid
+   sudo apt remove waydroid -y
    notify-send "My Waydroid Manager" "Done!"
 elif [ $(hostname) = 'ubuntu' ]; then
    notify-send "My Waydroid Manager" "Check Your Terminal"
    waydroid session stop
    sudo waydroid container stop
-   sudo apt remove waydroid
+   sudo apt remove waydroid -y
    notify-send "My Waydroid Manager" "Done!"
 elif [ $(hostname) = 'manjaro' ]; then
    echo "to be filled"
