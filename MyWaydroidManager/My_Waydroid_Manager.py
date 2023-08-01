9f012d6c8c980b3508fe179a559066889fdbc0fc
Keo <41642242+MadameMalady@users.noreply.github.com>
2023-07-31 22:20:47 -0400

    Update My_Waydroid_Manager.py
    Added support for custom lib-notify icons
    
    Added RobetofLocksley as a contributor in About

--- MyWaydroidManager/My_Waydroid_Manager.py
+++ MyWaydroidManager/My_Waydroid_Manager.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3                                                                            
 
-# v 0.2.0
+# v 0.2.1
 # © 2023 GPL 3.0
 
 
@@ -48,8 +48,7 @@ class MainWindow(Gtk.ApplicationWindow):
 
         # Add menu button to the header bar
         self.header.pack_end(self.hamburger)
- 
-         
+                
         # Main Application window size:      
         
         self.set_default_size(360, 720)
@@ -274,43 +273,33 @@ class MainWindow(Gtk.ApplicationWindow):
         self.about.set_transient_for(self)
         self.about.set_modal(self)
 
-        self.about.set_authors(["Kharra Oswin"])
-        self.about.set_copyright("© 2023 Kharra Oswin")
+        self.about.set_authors(["Kharra Oswin, RobetofLocksley"])
+        self.about.set_copyright("© 2023 GPL 3.0")
         self.about.set_license_type(Gtk.License.GPL_3_0)
         self.about.set_website("https://github.com/MadameMalady/My-Waydroid-Manager")
         self.about.set_website_label("Github")
-        self.about.set_version("0.2.0")
+        self.about.set_version("0.2.1")
         self.about.set_logo_icon_name("app.Keo.MyWaydroidManager")  
 
         self.about.set_visible(True)
-        
 
-#----------------------------------------------------------------------------------------------------------------------------------------
-           
-           
-    # This is where we will define what each button Does, and connect a function to each of them.
-    
-    #Button 3  
-    def Advanced_Options(self, button):
-        print("here's some more text, developer.")
-    
     # Button 3 def:
     def Install_Dependencies(self, button):
         # Multiline bash code
         dependencies_bash = """
         if [ $(hostname) = 'mobian' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 sudo apt install libnotify-bin
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'ubuntu' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 sudo apt install libnotify-bin
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'manjaro' ]; then
-                sudo pacman -Sy libnotify
-                notify-send "My Waydroid Manager" "Done!"
+                sudo pacman -Sy libnotify 
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'postmarketos*' ]; then
                 echo "to be implemented" 
@@ -322,11 +311,6 @@ class MainWindow(Gtk.ApplicationWindow):
         print("Installing Dependencies, please wait...")
         subprocess.run(["bash", "-c", dependencies_bash], check=True)
         
-        ## install dependencies for Waydroid
-        #print("Installing Dependencies, please wait...")
-        ##os.system("cd scripts && ./dependencies.sh")
-        #print(subprocess.run([os.path.join(self.current_dir, 'scripts', "dependencies.sh")], check=True))
-        
     # Button 4 def:    
     def Install_Application(self,button):
         # Multiline bash code
@@ -335,7 +319,7 @@ class MainWindow(Gtk.ApplicationWindow):
 
         if [ $(hostname) = 'mobian' ]; then
                 
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 
                 # copy application directory
                 mkdir -p /home/$user/MyWaydroidManager
@@ -345,12 +329,15 @@ class MainWindow(Gtk.ApplicationWindow):
                 sudo cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons/hicolor/scalable/apps
                 cp -p app.Keo.MyWaydroidManager.svg ~/.local/share/icons
                 sudo cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons
+                cd symbolic
+                sudo cp -p my-waydroid-manager-notify.svg /usr/share/icons/hicolor/scalable/status/
+                cd ..
                 cd ..
                 cd  ~/.local/share/applications 
                 touch MyWaydroidManager.desktop
                 echo "
         [Desktop Entry]
-        Version=0.1.9
+        Version=0.2.1
         Name=My Waydroid Manager
         Comment=Manage Waydroid installations easily
         Exec=/usr/bin/env python3 /home/$user/MyWaydroidManager/My_Waydroid_Manager.py
@@ -358,11 +345,11 @@ class MainWindow(Gtk.ApplicationWindow):
         X-Purism-FormFactor=Workstation;Mobile;
         Terminal=true
         Type=Application">MyWaydroidManager.desktop
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'ubuntu' ]; then
                 
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 
                 # copy application directory
                 mkdir -p /home/$user/MyWaydroidManager
@@ -372,12 +359,15 @@ class MainWindow(Gtk.ApplicationWindow):
                 sudo cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons/hicolor/scalable/apps
                 cp -p app.Keo.MyWaydroidManager.svg ~/.local/share/icons
                 sudo cp -p app.Keo.MyWaydroidManager.svg /usr/share/icons
+                cd symbolic
+                sudo cp -p my-waydroid-manager-notify.svg /usr/share/icons/hicolor/scalable/status/
+                cd ..
                 cd ..
                 cd  ~/.local/share/applications 
                 touch MyWaydroidManager.desktop
                 echo "
         [Desktop Entry]
-        Version=0.1.9
+        Version=0.2.1
         Name=My Waydroid Manager
         Comment=Manage Waydroid installations easily
         Exec=/usr/bin/env python3 /home/$user/MyWaydroidManager/My_Waydroid_Manager.py
@@ -385,7 +375,7 @@ class MainWindow(Gtk.ApplicationWindow):
         X-Purism-FormFactor=Workstation;Mobile;
         Terminal=true
         Type=Application">MyWaydroidManager.desktop
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'manjaro' ]; then
                 echo "to be implemented"
@@ -398,28 +388,24 @@ class MainWindow(Gtk.ApplicationWindow):
         """
         print("This will install all the required files locally, in the future you may launch from the app's icon")
         subprocess.run(["bash", "-c", install_application_bash], check=True)
-        ## install files locally as an application
-        #print("This will install all the required files locally, in the future you may launch from the app's icon")
-        ##os.system(f"cd {os.path.join(self.current_dir, 'scripts')} && ./install_application.sh")
-        #print(subprocess.run([os.path.join(self.current_dir, 'scripts', "install_application.sh")], check=True))
             
     # Button 5 def:
     def Purge_Waydroid_Installation(self, button):
         # Multiline bash code
         purge_bash = """
         if [ $(hostname) = 'mobian' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 waydroid session stop
                 sudo waydroid container stop
                 sudo apt remove waydroid -y
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'ubuntu' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 waydroid session stop
                 sudo waydroid container stop
                 sudo apt remove waydroid -y
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'manjaro' ]; then
                 echo "to be implemented"
@@ -432,24 +418,20 @@ class MainWindow(Gtk.ApplicationWindow):
         """
         print("This will completely remove waydroid")
         subprocess.run(["bash", "-c", purge_bash], check=True)
-        ## Purge Waydroid Installation
-        #print("This will completely remove waydroid")
-        ##os.system(f"cd {os.path.join(self.current_dir, 'scripts')} && ./purge.sh")
-        #print(subprocess.run([os.path.join(self.current_dir, 'scripts', "purge.sh")], check=True))
       
     # Button 6 def:
     def Purge_Waydroid_Images(self, button):
         # Multiline bash code
         purge_images_bash = """
         if [ $(hostname) = 'mobian' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 sudo rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'ubuntu' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 sudo rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'manjaro' ]; then
                 echo "to be implemented"
@@ -462,35 +444,31 @@ class MainWindow(Gtk.ApplicationWindow):
         """
         print("This will delete all waydroid images and directories")
         subprocess.run(["bash", "-c", purge_images_bash], check=True)
-        ## Purge Waydroid Images
-        #print("This will delete all waydroid images and directories")
-        ##os.system(f"cd {os.path.join(self.current_dir, 'scripts')} && ./purge_images.sh")
-        #print(subprocess.run([os.path.join(self.current_dir, 'scripts', "purge_images.sh")], check=True))
         
     #Button 7 def:
     def Install_Waydroid(self, button):
         # Multiline bash code
         build_bash = """
         if [ $(hostname) = 'mobian' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 sudo apt install curl ca-certificates -y
                 curl https://repo.waydro.id | sudo bash
                 sudo apt install waydroid -y
-                notify-send "My Waydroid Manager" "Launch Waydroid from it's application icon, minimize osk"
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "You may now launch Waydroid from its application icon and pick an image type."
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'ubuntu' ]; then
-                notify-send "My Waydroid Manager" "Check Your Terminal"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
                 sudo apt install curl ca-certificates -y
                 curl https://repo.waydro.id | sudo bash
                 sudo apt install waydroid -y
-                notify-send "My Waydroid Manager" "Launch Waydroid from it's application icon, minimize osk"
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "You may now launch Waydroid from its application icon and pick an image type."
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'manjaro' ]; then
                 sudo pacman -Syyu waydroid
                 waydroid
-                notify-send "My Waydroid Manager" "Done!"
+                notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
                 echo "Done!"
         elif [ $(hostname) = 'postmarketos*' ]; then
                 echo "Sorry, this option's still a work in progress"
@@ -500,43 +478,39 @@ class MainWindow(Gtk.ApplicationWindow):
         """
         print("Installing Waydroid, please wait..")
         subprocess.run(["bash", "-c", build_bash], check=True)
-        ## run 'build.sh' to try and build or install waydroid
-        #print("Installing Waydroid, please wait..")
-        ##os.system(f"cd {os.path.join(self.current_dir, 'scripts')} && ./build.sh")
-        #print(subprocess.run([os.path.join(self.current_dir, 'scripts', "build.sh")], check=True))
 
     # Button 8 def:
     def Get_Registration_Code(self, button):
         # Multiline bash code
         Retrieve_Registration_Code_bash = """
         if [ $(hostname) = 'mobian' ]; then
-            notify-send "My Waydroid Manager" "Check Your Terminal"
+            notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
             read -p "You need to have ran Waydroid at least once and picked an image type to run this option, If you've not done so yet, please launch Waydroid"
             echo ("Hit ctrl+c to cancel or, wait 3 seconds to continue")
             sleep 3
             touch Registration_Code.txt
             sudo waydroid shell
             ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";" > Registration_Code.txt
-            notify-send "My Waydroid Manager" "Done!"
+            notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
             echo "Done!"
         elif [ $(hostname) = 'ubuntu' ]; then
-            notify-send "My Waydroid Manager" "Check Your Terminal"
+            notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
             read -p "You need to have ran Waydroid at least once and picked an image type to run this option, If you've not done so yet, please launch Waydroid"
             echo ("Hit ctrl+c to cancel or, wait 3 seconds to continue")
             sleep 3
             touch Registration_Code.txt
             sudo waydroid shell
             ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";" > Registration_Code.txt
-            notify-send "My Waydroid Manager" "Done!"
+            notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
             echo "Done!"
         elif [ $(hostname) = 'manjaro' ]; then
-            notify-send "My Waydroid Manager" "Check Your Terminal"
+            notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Please check your Terminal"
             read -p "You need to have ran Waydroid at least once and picked an image type to run this option, If you've not done so yet, please launch Waydroid"
             echo ("Hit ctrl+c to cancel or, wait 3 seconds to continue")
             touch Registration_Code.txt
             sudo waydroid shell
             ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";" > Registration_Code.txt
-            notify-send "My Waydroid Manager" "Done!"
+            notify-send -i /usr/share/icons/hicolor/scalable/status/my-waydroid-manager-notify.svg "My Waydroid Manager" "Done!"
             echo "Done!"
         elif [ $(hostname) = 'postmarketos*' ]; then
             echo "Sorry, this option's still a work in progress"
@@ -546,14 +520,6 @@ class MainWindow(Gtk.ApplicationWindow):
         """
         print("Installing Waydroid, please wait..")
         subprocess.run(["bash", "-c", Retrieve_Registration_Code_bash], check=True)
-        ## run 'Retrieve_Registration_Code.sh' to write your registration code to a text file
-        #os.system("cd ~/MyWaydroidManager/scripts && ./Retrieve_Registration_Code.sh")
-        
-
-#----------------------------------------------------------------------------------------------------------------------------------------
-
-
-    # This tells the app how to open Various Windows, And close them.
 
     # def on activate:
     
